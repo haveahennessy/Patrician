b81a55afcdab683e07c81466f8217d7a71b756f3
Matt Isaacs <misaacs@gilt.com>
2015-04-02 23:48:27 -0400

    Basic testing.

--- RadonTests/RadonTests.swift
+++ RadonTests/RadonTests.swift
@@ -20,35 +20,46 @@ class RadonTests: XCTestCase {
     }
     
     func testInsert() {
-        let tree: RTree<Int> = RTree.emptyTree()
+        var tree: RadixTree<Int> = RadixTree.emptyTree()
         tree.insert("wolf", value: 3)
         tree.insert("world", value: 2)
-        tree.insert("wacko", value: 1)
+        tree.insert("bear", value: 1)
         tree.insert("wacker", value: 5)
 
-        let wacker = tree.lookup("wacker")
+        let wacker = tree["wacker"]
+        let wolf = tree["wolf"]
+        let world = tree["world"]
+        let bear = tree["bear"]
+
+        XCTAssert(bear == 1, "Lookup failed.")
+        XCTAssert(world == 2, "Lookup failed.")
+        XCTAssert(wolf == 3, "Lookup failed.")
+        XCTAssert(wacker == 5, "Lookup failed.")
         XCTAssert(tree.count == 4, "Size mismatch.")
     }
 
     func testDeleteSizeOne() {
-        let tree: RTree<Int> = RTree.emptyTree()
+        var tree: RadixTree<Int> = RadixTree.emptyTree()
         tree.insert("wolf", value: 3)
         XCTAssert(tree.count == 1, "Size mismatch.")
         tree.delete("w")
+        XCTAssert(tree["wolf"] == 3, "Missing value.")
         XCTAssert(tree.count == 1, "Size mismatch.")
         tree.delete("wolf")
+        XCTAssertNil(tree["wolf"], "Delete failed.")
         XCTAssert(tree.count == 0, "Size mismatch.")
     }
 
-    //Broken
     func testDelete() {
-        let tree: RTree<Int> = RTree.emptyTree()
+        var tree: RadixTree<Int> = RadixTree.emptyTree()
         tree.insert("wolf", value: 3)
         tree.insert("world", value: 2)
         XCTAssert(tree.count == 2, "Size mismatch.")
         tree.delete("w")
         XCTAssert(tree.count == 2, "Size mismatch.")
         tree.delete("wolf")
+        XCTAssert(tree["world"] == 2, "Missing value.")
+        XCTAssertNil(tree["wolf"], "Delete failed.")
         XCTAssert(tree.count == 1, "Size mismatch.")
     }
     
