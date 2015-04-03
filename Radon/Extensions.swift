e8f63e5763e5162dde65685117e26084e897df22
Matt Isaacs <misaacs@gilt.com>
2015-04-02 23:43:48 -0400

    Extensions.

---
+++ Radon/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  Radon
+//
+//  Created by Matt Isaacs.
+//  Copyright (c) 2015 Matt Isaacs. All rights reserved.
+//
+
+extension String {
+    var first: Character? {
+        if countElements(self) > 0 {
+            return self[self.startIndex]
+        }
+        return nil
+    }
+}
+
+extension ContiguousArray {
+    var slice: Slice<T> {
+        return self[0..<self.endIndex]
+    }
+}
